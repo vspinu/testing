4891859ac83a870403f61c7d8728130879872838
Stefanie LaZerte <steffi@steffi.ca>
2017-11-02 08:50:23 -0500

    More tests

--- tests/testthat/test_lubridate.R
+++ tests/testthat/test_lubridate.R
@@ -1,30 +1,75 @@
-context("lubridate intervals")
+context("lubridate timezones")
 
-
-test_that("lubridate intervals handle Etc timezones", {
-  t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "Etc/GMT+8")
-  i1 <- lubridate::interval(as.POSIXct("2000-01-01 00:00:00"), as.POSIXct("2017-01-01 00:00:00"))
-  i2 <- lubridate::interval(as.POSIXct("2000-01-01 00:00:00"), as.POSIXct("2015-01-01 00:00:00"))
-
-  expect_true(lubridate::`%within%`(t1, i1))
-  expect_false(lubridate::`%within%`(t1, i2))
-})
-
-test_that("lubridate with_tz UTC", {
+test_that("with_tz UTC", {
   t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "Etc/GMT+8")
   expect_equal(lubridate::with_tz(t1, tzone = "UTC"),
                as.POSIXct("2016-09-01 17:00:00", tz = "UTC"))
 })
 
-test_that("lubridate with_tz America/Vancouver", {
+test_that("with_tz America/Vancouver", {
   t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "Etc/GMT+8")
   expect_equal(lubridate::with_tz(t1, tzone = "America/Vancouver"),
                as.POSIXct("2016-09-01 10:00:00", tz = "America/Vancouver"))
 })
 
-test_that("lubridate with_tz Etc/GMT+8", {
+test_that("with_tz Etc/GMT+8", {
   t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "UTC")
   expect_equal(lubridate::with_tz(t1, tzone = "Etc/GMT+8"),
                as.POSIXct("2016-09-01 01:00:00", tz = "Etc/GMT+8"))
 })
 
+
+test_that("with_tz America/New_York", {
+  t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "UTC")
+  expect_equal(lubridate::with_tz(t1, tzone = "America/New_York"),
+               as.POSIXct("2016-09-01 05:00:00", tz = "America/New_York"))
+})
+
+test_that("with_tz Europe/Amsterdam", {
+  t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "UTC")
+  expect_equal(lubridate::with_tz(t1, tzone = "Europe/Amsterdam"),
+               as.POSIXct("2016-09-01 11:00:00", tz = "Europe/Amsterdam"))
+})
+
+test_that("with_tz America/New_York", {
+  t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "UTC")
+  expect_equal(lubridate::with_tz(t1, tzone = "EST"),
+               as.POSIXct("2016-09-01 04:00:00", tz = "EST"))
+})
+
+test_that("with_tz Europe/Amsterdam", {
+  t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "UTC")
+  expect_equal(lubridate::with_tz(t1, tzone = "Europe/Amsterdam"),
+               as.POSIXct("2016-09-01 11:00:00", tz = "Europe/Amsterdam"))
+})
+
+test_that("force_tz with_tz UTC", {
+  t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "Etc/GMT+8")
+  expect_equal(lubridate::force_tz(t1, tzone = "UTC"),
+               as.POSIXct("2016-09-01 09:00:00", tz = "UTC"))
+})
+
+test_that("force_tz with_tz America/Vancouver", {
+  t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "Etc/GMT+8")
+  expect_equal(lubridate::force_tz(t1, tzone = "America/Vancouver"),
+               as.POSIXct("2016-09-01 09:00:00", tz = "America/Vancouver"))
+})
+
+test_that("force_tz Etc/GMT+8", {
+  t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "UTC")
+  expect_equal(lubridate::force_tz(t1, tzone = "Etc/GMT+8"),
+               as.POSIXct("2016-09-01 09:00:00", tz = "Etc/GMT+8"))
+})
+
+
+test_that("force_tz America/New_York", {
+  t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "UTC")
+  expect_equal(lubridate::force_tz(t1, tzone = "America/New_York"),
+               as.POSIXct("2016-09-01 09:00:00", tz = "America/New_York"))
+})
+
+test_that("force_tz Europe/Amsterdam", {
+  t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "UTC")
+  expect_equal(lubridate::force_tz(t1, tzone = "Europe/Amsterdam"),
+               as.POSIXct("2016-09-01 09:00:00", tz = "Europe/Amsterdam"))
+})
