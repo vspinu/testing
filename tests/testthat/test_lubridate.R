44b6bb31cd59bbaf2ee003fa47851659c62dae85
Stefanie LaZerte <steffi@steffi.ca>
2017-11-01 19:18:23 -0500

    v1.6.0

--- tests/testthat/test_lubridate.R
+++ tests/testthat/test_lubridate.R
@@ -12,20 +12,19 @@ test_that("lubridate intervals handle Etc timezones", {
 
 test_that("lubridate with_tz UTC", {
   t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "Etc/GMT+8")
-  expect_silent(lubridate::with_tz(t1, tzone = "UTC"))
+  expect_equal(lubridate::with_tz(t1, tzone = "UTC"),
+               as.POSIXct("2016-09-01 17:00:00", tz = "UTC"))
 })
 
 test_that("lubridate with_tz America/Vancouver", {
   t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "Etc/GMT+8")
-  expect_silent(lubridate::with_tz(t1, tzone = "America/Vancouver"))
-})
-
-test_that("lubridate with_tz America/Toronto", {
-  t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "Etc/GMT+8")
-  expect_silent(lubridate::with_tz(t1, tzone = "America/Toronto"))
+  expect_equal(lubridate::with_tz(t1, tzone = "America/Vancouver"),
+               as.POSIXct("2016-09-01 10:00:00", tz = "America/Vancouver"))
 })
 
 test_that("lubridate with_tz Etc/GMT+8", {
   t1 <- as.POSIXct("2016-09-01 09:00:00", tz = "UTC")
-  expect_silent(lubridate::with_tz(t1, tzone = "Etc/GMT+8"))
+  expect_equal(lubridate::with_tz(t1, tzone = "Etc/GMT+8"),
+               as.POSIXct("2016-09-01 01:00:00", tz = "Etc/GMT+8"))
 })
+
